16532b23e6f9eef8cf97faf8ccc8461bf99917ad
Keyoonz <bonnardmae@gmail.com>
2025-11-18 17:40:31 +0100

    changing template

--- lua/nvim-kolorz/colors_template.lua
+++ lua/nvim-kolorz/colors_template.lua
@@ -1,24 +1,25 @@
 return {
-	-- Backgrounds & foregrounds
-	c_normal_fg = "{{colors.primary.default.hex}}",
-	c_normal_bg = "{{colors.background.default.hex}}",
+    -- Backgrounds & foregrounds
+    c_normal_fg = "{{colors.on_surface.default.hex}}",
+    c_normal_bg = "{{colors.surface.default.hex}}",
 
-	-- Syntax groups
-	c_comment = "{{colors.on_primary.default.hex}}",
-	c_string = "{{colors.tertiary.default.hex}}",
-	c_identifier = "{{colors.primary.default.hex}}",
-	c_function = "{{colors.secondary.default.hex}}",
-	c_statement = "{{colors.primary.default.hex}}",
-	c_keyword = "{{colors.primary.default.hex}}",
-	c_type = "{{colors.primary.default.hex}}",
-	c_error = "{{colors.error.default.hex}}",
-	c_visual = "{{colors.primary.default.hex}}",
+    -- Syntax groups
+    c_comment      = "{{colors.surface_variant.default.hex}}",
+    c_string       = "{{colors.tertiary.default.hex}}",
+    c_identifier   = "{{colors.on_surface.default.hex}}",
+    c_function     = "{{colors.primary.default.hex}}",
+    c_statement    = "{{colors.secondary.default.hex}}",
+    c_keyword      = "{{colors.primary_fixed.default.hex}}",
+    c_type         = "{{colors.secondary_fixed.default.hex}}",
+    c_error        = "{{colors.error.default.hex}}",
+    c_visual       = "{{colors.surface_container_high.default.hex}}",
 
-	-- Line numbers
-	c_line_nr = "{{colors.on_primary.default.hex}}",
-	c_cursor_line_nr = "{{colors.primary.default.hex}}",
+    -- Line numbers
+    c_line_nr         = "{{colors.on_surface_variant.default.hex}}",
+    c_cursor_line_nr  = "{{colors.primary.default.hex}}",
 
-	-- Statusline
-	c_status_line_fg = "{{colors.primary.default.hex}}",
-	c_status_line_bg = "{{colors.background.default.hex}}",
+    -- Statusline
+    c_status_line_fg = "{{colors.on_surface_variant.default.hex}}",
+    c_status_line_bg = "{{colors.surface_container_low.default.hex}}",
 }
+
