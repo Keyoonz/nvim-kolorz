c4d81ceac64fb22bca07fe5ef3473e3a23b5e7b9
Keyoonz <bonnardmae@gmail.com>
2025-11-18 15:27:30 +0100

    template for matugen

---
+++ lua/nvim-kolorz/colors_template.lua
@@ -0,0 +1,24 @@
+return {
+	-- Backgrounds & foregrounds
+	c_normal_fg = "{{colors.primary.default.hex}}",
+	c_normal_bg = "{{colors.background.default.hex}}",
+
+	-- Syntax groups
+	c_comment = "{{colors.on_primary.default.hex}}",
+	c_string = "{{colors.tertiary.default.hex}}",
+	c_identifier = "{{colors.primary.default.hex}}",
+	c_function = "{{colors.secondary.default.hex}}",
+	c_statement = "{{colors.primary.default.hex}}",
+	c_keyword = "{{colors.primary.default.hex}}",
+	c_type = "{{colors.primary.default.hex}}",
+	c_error = "{{colors.error.default.hex}}",
+	c_visual = "{{colors.primary.default.hex}}",
+
+	-- Line numbers
+	c_line_nr = "{{colors.on_primary.default.hex}}",
+	c_cursor_line_nr = "{{colors.primary.default.hex}}",
+
+	-- Statusline
+	c_status_line_fg = "{{colors.primary.default.hex}}",
+	c_status_line_bg = "{{colors.background.default.hex}}",
+}
